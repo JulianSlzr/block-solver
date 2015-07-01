e248c38b5509a7640381d4bf200a2324a0cc2979
Harry <harry@harry-Inspiron-N5050.(none)>
2015-07-01 16:27:48 +0100

    Added a label getter to puzzle.Block, created matrix visualiser and made puzzleGenerator work properly

---
+++ matrixvisualiser.py
@@ -0,0 +1,39 @@
+from tkinter import Tk, Frame, BOTH, Canvas
+
+"""To use, call matrixvisualiser.drawMatrix(matrix,colours). 'matrix' is the matrix to be shown, 'colours' is a
+    dictionary between matrix entries and colours eg {0:"#ffffff, 1:"#000000"} colours all zeros as white and ones as black"""
+
+boxWidth = 25
+boxHeight = 25
+outlineWidth=0
+outlineColour="black"
+
+class Drawer(Frame):
+    """The object matrices will be drawn in. A matrix here is a list of lists, assumed to be rectangular and non-empty
+        with integer entries. Numpy arrays also work."""
+    def __init__(self, parent, matrix, colours):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.initUI(matrix,colours)
+
+    def initUI(self, matrix, colours):
+        self.parent.title("Matrix Visualiser")
+        self.pack(fill=BOTH, expand=1)
+        self.draw(matrix,colours)
+
+    def draw(self,matrix, colours):
+        canvas = Canvas(self, background="white")
+        row = len(matrix)
+        col = len(matrix[0])
+        for i in range(0,row):
+            for j in range(0,col):
+                canvas.create_rectangle(i*boxWidth, j*boxHeight, (i+1)*boxWidth, (j+1)*boxHeight, fill=colours[matrix[i][j]],
+                                        outline=outlineColour, width=outlineWidth)
+
+        canvas.pack(fill=BOTH, expand=1)
+
+def drawMatrix(matrix, colours):
+    root = Tk()
+    root.geometry(str(boxWidth*len(matrix))+"x"+str(boxHeight*len(matrix[0]))+"+100+100")
+    drawer = Drawer(root,matrix,colours)
+    root.mainloop()
--- puzzleGenerator.py
+++ puzzleGenerator.py
@@ -3,15 +3,9 @@ import numpy as np
 import random
 import math
 
-"""Create a small library of 'normal' pieces, just tetronimoes for now. Not used yet"""
-standardPieces = []
-standardPieces.append(pz.Block([[1,1],[1,1]]))  # 2x2 square
-standardPieces.append(pz.Block([[1,1,1],[0,1,0]]))  # Small T
-standardPieces.append(pz.Block([[1,0,0],[1,1,1]]))  # Small L
-standardPieces.append(pz.Block([[0,0,1],[1,1,1]]))  # Small L
-standardPieces.append(pz.Block([[0,1,1],[1,1,0]]))  # Small Z
-standardPieces.append(pz.Block([[1,1,0],[0,1,1]]))  # Small Z
-standardPieces.append(pz.Block([[1,1,1,1]]))  # Small straight
+import matrixvisualiser as mv
+
+remove1x1 = True
 
 class Point:
 
@@ -24,7 +18,6 @@ class Point:
             return True
         else:
             return False
-
 
 def growPiecesGen(row,col,avgSize):
     """Generate a puzzle by growing pieces up to a size of avgSize"""
@@ -39,9 +32,16 @@ def growPiecesGen(row,col,avgSize):
         During construction represent a piece by using a list of coordinates for the squares present"""
 
         tempBlock = [nextSquare]
-        board[nextSquare.x,nextSquare.y] = pieceNumber
+        board[nextSquare.x, nextSquare.y] = pieceNumber
         numSquares = 1
-        pieceSize = avgSize  # Can add some noise here to reduce the uniformity
+
+        """----------Specify the distribution of piece sizes here----------"""
+        variation = 0.33
+        proportion = 1 - variation + 2*variation*random.random()
+        pieceSize = math.floor(proportion*avgSize)
+        #pieceSize = avgSize
+        #pieceSize = 2 + math.floor(random.random()*6)
+
         for i in range (0,pieceSize-1):
             viable = False
             attempts = 0
@@ -117,101 +117,117 @@ def growPiecesGen(row,col,avgSize):
             if flag:
                 break
 
+
+
+    if remove1x1:
+        board = removeLittleSquares(board, pieces)
+
     # Print here to show a completed board
-    print(board)
+    #print(board)
 
-    correctLittlePieces = True
-    if correctLittlePieces:
-        """If a piece is 'little' then extrude a neighbouring piece into each of its squares"""
+    #return pz.Puzzle(row,col,frozenset(pieces))
+    return board
+
+def removeLittleSquares(board, pieces):
+    rows = board.shape[0]
+    cols = board.shape[1]
+    hasChanged = False
+    justStarted = True
+    while justStarted or hasChanged: # Unfortunately, cannot iterate just over 'pieces' as the set changes in each loop
+        justStarted = False
+        hasChanged = False
         for piece in pieces:
-            typeytypey=1
+            if piece.rows == 1 and piece.cols == 1:
+                hasChanged = True
+                # So we know we're working with a trivial square
+                pieceNumber = piece.config[0,0]
+                # Find in the board
+                location = Point(-1,-1)
+                for i in range(0,rows):
+                    for j in range(0,cols):
+                        if board[i, j] == pieceNumber:
+                           location = Point(i,j)
 
-    return pz.Puzzle(row,col,frozenset(pieces))
+                # Find a direction we can glue the square
+                successfulGluing = False
+                while not successfulGluing:
+                   # Randomly pick a direction to glue
+                    direction = math.floor(random.random()*4)
+                    if direction == 0:
+                        if location.x > 0:
+                            # So can glue to the left. Get the label of that piece
+                            newNumber = board[location.x-1,location.y]
+                            successfulGluing=True
+                    elif direction == 1:
+                        if location.x <rows-1:
+                            newNumber = board[location.x+1,location.y]
+                            successfulGluing=True
+                    elif direction == 2:
+                        if location.y > 0:
+                            newNumber = board[location.x,location.y-1]
+                            successfulGluing=True
+                    elif direction == 3:
+                        if location.y <cols-1:
+                            newNumber = board[location.x,location.y+1]
+                            successfulGluing=True
 
-def randomGluingGen(row,col,p):
-    """Generate a puzzle by randomly choosing whether to glue adjacent squares. This turns out to lead to not very nice
-    puzzles"""
-    board = np.zeros((row,col))
-    pieces = set([])
-    nextSquare = Point(0,0)
-    completed = False
-    pieceNumber = 1  # Identifier for each piece
-    rightGluings = np.random.rand(row-1, col)
-    downGluings = np.random.rand(row, col-1)
+                #Find the piece
+                newPiece = 0
+                for otherPiece in pieces:
+                    if otherPiece.getLabel() == newNumber:
+                        newPiece = otherPiece
+                        break
 
-    while not completed:
-        """We start growing the block in 'next' position"""
-        tempBlock = [nextSquare]
-        newSquares = [nextSquare]
-        nextNewSquares = []  # Store what the next set of new squares will be without messing with current set
-        board[nextSquare.x, nextSquare.y] = pieceNumber
-        while newSquares != []:
-            # Search for glued squares not yet in the block
-            for square in newSquares:
-                x = square.x
-                y = square.y
-                if x < row - 1:
-                    if rightGluings[x,y]<p and Point(x+1,y) not in tempBlock:
-                        tempBlock.append(Point(x + 1, y))
-                        nextNewSquares.append(Point(x + 1, y))
-                        board[x + 1, y] = pieceNumber
+                # Now find the (x,y) coordinate of the new piece
+                firstSquare = -1
+                for i in range(0,newPiece.rows):
+                    if newPiece.config[i,0]!=0:
+                        firstSquare = i
+                        break
+                origin = Point(-1,-1)
+                flag = 0
+                for j in range(0,cols):
+                    for i in range(0,rows):
+                        if board[i,j]==newNumber:
+                            origin = Point(i-firstSquare,j)
+                            flag = 1
+                            break
+                    if flag == 1:
+                        break
 
+                relativePosition = Point(location.x-origin.x,location.y-origin.y)
 
-                if x > 0:
-                    if rightGluings[x-1,y]<p and Point(x - 1, y) not in tempBlock:
-                        tempBlock.append(Point(x - 1, y))
-                        nextNewSquares.append(Point(x-1,y))
-                        board[x - 1, y] = pieceNumber
+                r = newPiece.rows
+                c = newPiece.cols
+                if relativePosition.x >= r:
+                    finalPiece = pz.Block(np.zeros((r+1,c)))
+                    finalPiece.config[0:r,0:c] = newPiece.config[0:r,0:c]
+                    finalPiece.config[relativePosition.x,relativePosition.y] = newNumber
+                elif relativePosition.x < 0:
+                    finalPiece = pz.Block(np.zeros((r+1,c)))
+                    finalPiece.config[1:r+1,0:c] = newPiece.config[0:r,0:c]
+                    finalPiece.config[relativePosition.x+1,relativePosition.y] = newNumber
+                elif relativePosition.y >= c:
+                    finalPiece = pz.Block(np.zeros((r,c+1)))
+                    finalPiece.config[0:r,0:c] = newPiece.config[0:r,0:c]
+                    finalPiece.config[relativePosition.x,relativePosition.y] = newNumber
+                elif relativePosition.y < 0:
+                    finalPiece = pz.Block(np.zeros((r,c+1)))
+                    finalPiece.config[0:r,1:c+1] = newPiece.config[0:r,0:c]
+                    finalPiece.config[relativePosition.x,relativePosition.y+1] = newNumber
+                else:
+                    finalPiece = newPiece
+                    finalPiece.config[relativePosition.x,relativePosition.y] = newNumber
 
-                if y < col - 1:
-                    if downGluings[x,y]<p and Point(x, y + 1) not in tempBlock:
-                        tempBlock.append(Point(x, y + 1))
-                        nextNewSquares.append(Point(x, y + 1))
-                        board[x, y + 1] = pieceNumber
+                #Correct the board
+                board[location.x,location.y] = newNumber
 
-                if y > 0:
-                    if downGluings[x,y-1]<p and Point(x, y - 1) not in tempBlock:
-                        tempBlock.append(Point(x, y - 1))
-                        nextNewSquares.append(Point(x, y - 1))
-                        board[x, y - 1] = pieceNumber
-
-            newSquares = nextNewSquares
-            nextNewSquares = []
-
-        # Construct the block with its regular representation
-        minX = float('inf')
-        maxX = -float('inf')
-        minY = float('inf')
-        maxY = -float('inf')
-        for square in tempBlock:
-            minX = min(minX,square.x)
-            maxX = max(maxX,square.x)
-            minY = min(minY,square.y)
-            maxY = max(maxY,square.y)
-
-        config = np.zeros((maxX-minX+1,maxY-minY+1))
-
-        for square in tempBlock:
-            config[square.x-minX,square.y-minY] = pieceNumber
-
-        pieces.add(pz.Block(config))
-
-        # Prepare for the next pass
-        pieceNumber +=1
-        # Find the next square we need to fill, if any
-        completed = True
-        flag = 0
-        for i in range(0,row):
-            for j in range(0,col):
-                if board[i,j] == 0:
-                    completed = False
-                    nextSquare = Point(i,j)
-                    flag = 1
-                    break
-
-            if flag:
+                #Correct the piece list
+                pieces.remove(piece)
+                pieces.remove(newPiece)
+                pieces.add(finalPiece)
                 break
 
-    # Print here to show a completed board
-    print(board)
-
+    #Should return pieces in the end
+    return board
+    #return pieces
